22db4b217c41a99b63a17aca968348d9a4f080a5
a21alejandrovp <a21alejandrovp@iessanclemente.net>
2024-10-08 10:24:58 +0200

    añadidos objetos.html y objetos.js

---
+++ objetos.js
@@ -0,0 +1,66 @@
+// EJERCICIOS OBXECTOS.PDF (https://mestre.iessanclemente.net/pluginfile.php/2048/mod_resource/content/63/Obxectos.pdf)
+
+// STRINGS - EJERCICIO 1 - PÁGINA 4
+// --------------------------------
+
+// const cadea = "desenvolvemento web";
+// const novaCadea = `${cadea.at(0).toUpperCase()}esenvolvemento web`;
+// console.log(novaCadea);
+
+// const cadea = "desenvolvemento web";
+// const novaCadea = cadea.replace("d", "D");
+// console.log(novaCadea);
+
+// STRINGS - EJERCICIO 2 - PÁGINA 4
+// --------------------------------
+
+// function reverseString(cadea) {
+//   let novaCadea = ``;
+
+//   for (let i = cadea.length - 1; i >= 0; i--) {
+//     novaCadea = `${novaCadea}${cadea.at(i)}`;
+//   }
+
+//   return novaCadea;
+// }
+
+// const nome = `Alejandro`;
+// console.log(reverseString(nome));
+
+// STRINGS - EJERCICIO 3 - PÁGINA 4
+// --------------------------------
+
+// function enmascarar(cadea) {
+//   let novaCadea = "";
+//   let i = cadea.length;
+
+//   for (let j = 0; j < i; j++) {
+//     if (j < i - 4) {
+//       novaCadea = `${novaCadea}*`;
+//     } else {
+//       novaCadea = `${novaCadea}${cadea.at(j)}`;
+//     }
+//   }
+//
+//   return novaCadea;
+// }
+//
+// const numeros = "49665110";
+// console.log(enmascarar(numeros));
+
+// NUMBER - EJERCICIO 1 - PÁGINA 5
+// -------------------------------
+
+// let num = 49665110;
+// let cadeaNum = num.toString();
+// console.log(cadeaNum.length);
+
+// MATH - EJERCICIO 1 - PÁGINA 6
+// -----------------------------
+
+// console.log(Math.ceil(Math.random() * 3));
+
+// MATH - EJERCICIO 2 - PÁGINA 6
+// -----------------------------
+
+// console.log(Math.ceil(Math.random() * 2 + 1));
