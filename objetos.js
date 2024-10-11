ae9ac2d40fc7ae57b856e41ce9dab4196735fbf1
a21alejandrovp <a21alejandrovp@iessanclemente.net>
2024-10-11 10:25:38 +0200

    modificado objetos.js (añadidos ejercicios DATE y ARRAYS)

--- objetos.js
+++ objetos.js
@@ -56,14 +56,14 @@
 // console.log(cadeaNum.length);
 
 // MATH - EJERCICIO 1.A - PÁGINA 6
-// -----------------------------
+// -------------------------------
 
-// console.log(Math.ceil(Math.random() * 3));
+// console.log(Math.floor(Math.random() * 4 ));
 
 // MATH - EJERCICIO 1.B - PÁGINA 6
-// -----------------------------
+// -------------------------------
 
-// console.log(Math.ceil(Math.random() * 2 + 1));
+// console.log(Math.floor(Math.random() * 3 + 1));
 
 // MATH - EJERCICIO 1.C - PÁGINA 6
 // -------------------------------
@@ -103,5 +103,90 @@
 
 // console.log(area(4));
 
+//
+//
+//
+//
+// POR HACER
+//
+//
+//
+//
+
 // DATE - EJERCICIO 1 - PÁGINA 8
 // -----------------------------
+
+// let data = new Date("2024-07-25");
+//
+// const nomesDias = [
+//   "Domingo",
+//   "Luns",
+//   "Martes",
+//   "Mércores",
+//   "Xoves",
+//   "Venres",
+//   "Sábado",
+// ];
+//
+// console.log(nomesDias[data.getDay()]);
+
+// DATE - EJERCICIO 2 - PÁGINA 8
+// -----------------------------
+
+// function numeroDiasMes(mes, ano) {
+//   let data = new Date(ano, mes, 0);
+//   return data.getDate();
+// }
+//
+// console.log(numeroDiasMes(2, 2024));
+
+// DATE - EJERCICIO 3 - PÁGINA 8
+// -----------------------------
+
+// function eFinSemana(data) {
+//   if (data.getDay() == 0 || data.getDay() === 6) return true;
+//   else return false;
+// }
+//
+// console.log(eFinSemana(new Date("2024-09-12")));
+
+// DATE - EJERCICIO 4 - PÁGINA 8
+// -----------------------------
+
+// function diasTranscurridos(data) {
+//   let primeiroDiaAno = new Date(data.getFullYear(), 0, 1);
+//
+//   let diferencia = data - primeiroDiaAno;
+//   return Math.floor(diferencia / (1000 * 60 * 60 * 24));
+// }
+//
+// console.log(diasTranscurridos(new Date(2024, 9, 11)));
+// console.log(diasTranscurridos(new Date(2024, 0, 11)));
+
+//
+//
+//
+//
+// POR HACER
+//
+//
+//
+//
+
+// ARRAYS - EJERCICIO 1 - PÁGINA 10
+// --------------------------------
+// POR TERMINAR
+
+// const numeros = [1, 3, 5, 1, 4, 1, 6, 8, 10, 1];
+//
+// function indices(elemento, arrayElementos) {
+//   let newArrayElementos = [];
+//
+//   for (let i = 0; i < arrayElementos.length; i++) {
+// if (arrayElementos[i] == elemento) {
+//   newArrayElementos[i] = i;
+// }
+//   }
+// }
+//
+// console.log(indices(1, numeros));
