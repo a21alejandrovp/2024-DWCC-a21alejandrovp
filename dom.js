918f46fabb6400dab6c0b68eb6405e194008b574
a21alejandrovp <a21alejandrovp@iessanclemente.net>
2024-11-12 10:21:23 +0100

    añadidos ejercicios DOM (dom.html, dom.js, dom2.html y dom2.js)

---
+++ dom.js
@@ -0,0 +1,49 @@
+// EJERCICIOS DOM.PDF (https://mestre.iessanclemente.net/pluginfile.php/2049/mod_resource/content/65/DOM.pdf)
+// ----------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------
+
+// PROPIEDADES DE UN NODO - EJERCICIO 1 - PÁGINA 16
+// ------------------------------------------------
+// ------------------------------------------------
+// ------------------------------------------------
+// ------------------------------------------------
+// ------------------------------------------------
+
+const body = document.querySelector("body");
+const form = body.querySelector("form");
+const formLabel = form.getElementsByTagName("label")[3];
+
+console.log(formLabel.innerHTML);
+console.log(formLabel.innerText);
+console.log(formLabel.textContent);
+
+// ------------------------------------------------
+
+const value = formLabel.querySelector("input").value;
+console.log(value);
+
+// ------------------------------------------------
+
+// const valueChecked = formLabel.
+// console.log(valueChecked);
+
+// ------------------------------------------------
+
+const ul = body.querySelector("ul");
+const liGroup = ul.getElementsByTagName("li");
+
+for (const li of liGroup) {
+  console.log(li.textContent);
+}
+
+// ------------------------------------------------
+
+console.log(liGroup.length);
+
+// ------------------------------------------------
+
+const div1 = body.querySelector("div");
+const div2 = div1.querySelector("div");
+console.log(div2.dataset.widgetName);
