069057ce9c443607abd6052d93c316b3318a279b
a21alejandrovp <a21alejandrovp@iessanclemente.net>
2025-02-11 10:22:07 +0100

    añadidos ejericios VUE

--- Vue/04-formularios01.js
+++ Vue/04-formularios01.js
@@ -2,6 +2,15 @@ import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 
 createApp({
   data() {
-    return {};
+    return {
+      texto: "",
+      checked: [],
+      // checked0: "false",
+      // checked1: "false",
+      // checked2: "false",
+      // checked3: "false",
+      picked: "",
+      selected: "",
+    };
   },
 }).mount("#div");
